84dffa5c44b11330daf007af8e1676ebea43acfb
aharshbe <a.catawampus@gmail.com>
2016-04-30 16:59:35 -0700

    Adding this before I break it trying to add the searchable xml stuff

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/ActivityTwo.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/ActivityTwo.java
@@ -1,7 +1,12 @@
 package com.example.austin.harrypotterappproject2;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.widget.SearchView;
+
 /**
  * Created by austin on 4/30/16.
  */
 public class ActivityTwo {
+
 }
