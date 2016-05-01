34320b7982b5c8812c846edda7def0bd3bf1dc92
aharshbe <a.catawampus@gmail.com>
2016-05-01 09:48:04 -0700

    Adding intent and button to easteregg

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/easteregg.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/easteregg.java
@@ -1,7 +1,9 @@
 package com.example.austin.harrypotterappproject2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class easteregg extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class easteregg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eastergg);
+    }
+
+    //Creates intent for user so they can go back to main screen
+
+    public void backtocategories(View view) {
+        Intent intent = new Intent(easteregg.this, MainActivity.class);
+        startActivity(intent);
     }
 }
