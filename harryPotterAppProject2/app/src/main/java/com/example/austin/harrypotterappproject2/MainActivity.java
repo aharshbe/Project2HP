46b775cdd2745aa51f9a79294056ade680514eae
aharshbe <a.catawampus@gmail.com>
2016-04-30 17:16:38 -0700

    Adding onclicks for books and quotes as well, some comments too

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/MainActivity.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/MainActivity.java
@@ -14,11 +14,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Creates an OnClick event for intent on the movies textView
+
+
     public void whenClickedMovies(View view) {
-        Toast.makeText(getApplicationContext(), "clicked HP Movies", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "clicked HP Movies", Toast.LENGTH_SHORT).show();
 
 //        Intent intent = new Intent(MainActivity.this, A2.class);
 //        startActivity(intent);
 
+    }
+
+    //Creates an OnClick event for intent on the books textView
+
+    public void whenClickedBooks(View view) {
+        Toast.makeText(getApplicationContext(), "clicked HP Books", Toast.LENGTH_SHORT).show();
+    }
+
+    //Creates an OnClick event for intent on the quotes textView
+
+    public void whenClickedQuotes(View view) {
+        Toast.makeText(getApplicationContext(), "clicked HP Quotes", Toast.LENGTH_SHORT).show();
     }
 }
