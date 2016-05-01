fe4d1c52c43e4c4510d2678be1e040fa12e6c2e8
aharshbe <a.catawampus@gmail.com>
2016-05-01 14:22:34 -0700

    Adds intent over from quotes to main acitvity

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/QuotesActivity.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/QuotesActivity.java
@@ -1,7 +1,9 @@
 package com.example.austin.harrypotterappproject2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class QuotesActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class QuotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotes);
+    }
+
+    //Adds intent over to main acitivty from quotes
+
+
+    public void clickingquotes(View view) {
+        Intent intent = new Intent(QuotesActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
