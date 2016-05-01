c7ed03f1deec6a8ae04440c45686d7b3019980f0
aharshbe <a.catawampus@gmail.com>
2016-04-30 17:51:10 -0700

    creating and verifying onClicks for movies

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main2Activity.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main2Activity.java
@@ -17,9 +17,21 @@ public class Main2Activity extends AppCompatActivity {
 
     }
 
-    //Creates an OnClick event for intent on the movies textView
+    //Creates an OnClick event for intent on the description of movie 1 textView
 
     public void whenClickedmovie1(View view) {
         Toast.makeText(getApplicationContext(), "clicked HP Movie 1", Toast.LENGTH_SHORT).show();
+    }
+
+    //Creates an OnClick event for intent the description of movie 2 textView
+
+    public void whenClickedmovie2(View view) {
+        Toast.makeText(getApplicationContext(), "clicked HP Movie 2", Toast.LENGTH_SHORT).show();
+    }
+
+    //Creates an OnClick event for intent the description of movie 3 textView
+
+    public void whenClickedmovie3(View view) {
+        Toast.makeText(getApplicationContext(), "clicked HP Movie 3", Toast.LENGTH_SHORT).show();
     }
 }
