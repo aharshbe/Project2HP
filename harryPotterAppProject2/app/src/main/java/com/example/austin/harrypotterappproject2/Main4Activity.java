3b883645ccf5e55b11fcd4480bd1642485fb9c23
aharshbe <a.catawampus@gmail.com>
2016-05-01 10:05:12 -0700

    Adds intents

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main4Activity.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main4Activity.java
@@ -1,5 +1,6 @@
 package com.example.austin.harrypotterappproject2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,17 @@ public class Main4Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main4);
     }
 
+    //Intents over to first activity as a back button
+
     public void clickingOnBook(View view) {
+        Intent intent = new Intent(Main4Activity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    //Will intent over to the acitivty that contains description
+
+    public void clickOnfirstBook(View view) {
+        Intent intent = new Intent(Main4Activity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
