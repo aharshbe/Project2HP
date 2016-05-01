50f59d908ef72bca44f86f5b0dd195f5cf925852
aharshbe <a.catawampus@gmail.com>
2016-04-30 17:13:24 -0700

    verfified that onClick works for movies

---
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/A2.java
@@ -0,0 +1,7 @@
+package com.example.austin.harrypotterappproject2;
+
+/**
+ * Created by austin on 4/30/16.
+ */
+public class A2 {
+}
--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/MainActivity.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.austin.harrypotterappproject2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void whenClickedMovies(View view) {
+        Toast.makeText(getApplicationContext(), "clicked HP Movies", Toast.LENGTH_LONG).show();
+
+//        Intent intent = new Intent(MainActivity.this, A2.class);
+//        startActivity(intent);
+
     }
 }
