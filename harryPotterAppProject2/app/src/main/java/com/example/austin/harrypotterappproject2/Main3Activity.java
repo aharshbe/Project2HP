2cd519df60c7d8c83657140c708e33104b171ff1
aharshbe <a.catawampus@gmail.com>
2016-04-30 18:36:38 -0700

    Adds the back button on a3 to the movies on a2

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main3Activity.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main3Activity.java
@@ -1,7 +1,9 @@
 package com.example.austin.harrypotterappproject2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_three);
+    }
+
+    //Creates intent for user so they can go back to movies screen
+
+    public void back(View view) {
+        Intent intent = new Intent(Main3Activity.this, Main2Activity.class);
+        startActivity(intent);
     }
 }
