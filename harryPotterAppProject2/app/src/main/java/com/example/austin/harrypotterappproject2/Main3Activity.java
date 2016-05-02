de39aba40f4f31b0e9fc8aa2d175fd5faa6ea520
aharshbe <a.catawampus@gmail.com>
2016-05-02 15:48:46 -0700

    Adding the descirption constructors for a3, which containt plot, runtime, date, topquote, and gross

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main3Activity.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main3Activity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 public class Main3Activity extends AppCompatActivity {
 
     //References all the textView which will pull that data from the database for each movie
-    TextView Plot, plotlistview, Date, runtime,  topquote, gross, datelistview, runtimelistview, topquotelistview, grosslistview;
+    TextView plot, date, runtime,  topquote, gross;
 
 
 
@@ -25,4 +25,15 @@ public class Main3Activity extends AppCompatActivity {
         Intent intent = new Intent(Main3Activity.this, Main2Activity.class);
         startActivity(intent);
     }
+
+    //Creates constructor for textViews which represent each of the values in the database for the descirption of the movies
+    public Main3Activity(TextView plot, TextView date, TextView runtime, TextView topquote, TextView gross) {
+        this.plot = plot;
+        this.date = date;
+        this.runtime = runtime;
+        this.topquote = topquote;
+        this.gross = gross;
+    }
 }
+
+
