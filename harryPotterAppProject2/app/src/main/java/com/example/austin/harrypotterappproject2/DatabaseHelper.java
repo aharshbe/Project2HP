790be59d82c22e34cf5d0205491f85d733bda330
aharshbe <a.catawampus@gmail.com>
2016-05-02 15:40:57 -0700

    Adding more database stuff, finished up adding for movies and movie descriptions

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/DatabaseHelper.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/DatabaseHelper.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.sql.Blob;
+
 /**
  * Created by austin on 5/2/16.
  */
@@ -13,15 +15,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Movies.db";
 
-    //Creates database
+    //Creates databases
     public static final String SQL_CREATE_MOVIE_TABLE =
             "CREATE TABLE movies ( id INTEGER PRIMARY KEY, movie TEXT, cover BLOB )";
     public static final String SQL_CREATE_MOVIEDESCRIPTION_TABLE =
             "CREATE TABLE movies ( id INTEGER PRIMARY KEY, plot TEXT, date TEXT, runtime TEXT, topquote TEXT, gross TEXT )";
-    //Delete database
-    public static final String SQL_DROP_MOVIEDESCRIPTION_TABLE = "DROP TABLE IF EXISTS movies";
-
-
+    //Delete databases
+    public static final String SQL_DROP_MOVIE_TABLE = "DROP TABLE IF EXISTS movies";
+    public static final String SQL_DROP_MOVIEDESCRIPTION_TABLE = "DROP TABLE IF EXISTS moviesdescirption";
 
 
     // override the SQLiteDatabase's constructor, onCreate, and onUpgrade
@@ -31,16 +32,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     //Creates the movies table when the data is created
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_MOVIE_TABLE);
         db.execSQL(SQL_CREATE_MOVIEDESCRIPTION_TABLE);
     }
 
     //Updates when data us upgraded and then deletes the movies table and recreates it
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DROP_MOVIE_TABLE);
         db.execSQL(SQL_DROP_MOVIEDESCRIPTION_TABLE);
         onCreate(db);
     }
 
-    public void insert(int id, String plot, String date, String runtime, String topquote, String gross){
+    public void insertmovies(int id, String movie, Blob cover){
+        // Get a reference to the database
+        SQLiteDatabase db = getWritableDatabase();
+
+        // create a new content value to store values
+        ContentValues values = new ContentValues();
+        values.put("id", id);
+        values.put("movie", movie);
+        values.put("cover", String.valueOf(cover));
+
+        // Insert the row into the movies table
+        db.insert("movies", null, values);
+    }
+
+    public void insertdescription(int id, String plot, String date, String runtime, String topquote, String gross){
         // Get a reference to the database
         SQLiteDatabase db = getWritableDatabase();
 
@@ -54,6 +71,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put("gross", gross);
 
         // Insert the row into the movies table
-        db.insert("movies", null, values);
+        db.insert("moviesdescirption", null, values);
     }
 }
