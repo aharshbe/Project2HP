246c094521878aeb58cb0635a1848d2278fac193
aharshbe <a.catawampus@gmail.com>
2016-05-02 15:12:20 -0700

    Adds some more information to the database

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/DatabaseHelper.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/DatabaseHelper.java
@@ -12,16 +12,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Movies.db";
 
+    //Creates database
+    public static final String SQL_CREATE_MOVIE_TABLE =
+            "CREATE TABLE movies ( id INTEGER PRIMARY KEY, plot TEXT, date TEXT, runtime TEXT, topquote TEXT, gross TEXT )";
+    //Delete database
+    public static final String SQL_DROP_MOVIE_TABLE = "DROP TABLE IF EXISTS movies";
+
+
+
+
     // override the SQLiteDatabase's constructor, onCreate, and onUpgrade
     public DatabaseHelper (Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    //Creates the movies table when the data is created
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_MOVIE_TABLE);
     }
 
+    //Updates when data us upgraded and then deletes the movies table and recreates it
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(SQL_DROP_MOVIE_TABLE);
+        onCreate(db);
     }
 }
