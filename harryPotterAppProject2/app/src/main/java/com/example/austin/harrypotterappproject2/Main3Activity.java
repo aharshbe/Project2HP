9fc3209a6decd093b33787023616906b66fc47ea
aharshbe <a.catawampus@gmail.com>
2016-05-02 15:29:25 -0700

    Adds references to all the textview on activity 3 that pull from the database

--- harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main3Activity.java
+++ harryPotterAppProject2/app/src/main/java/com/example/austin/harrypotterappproject2/Main3Activity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
+
+    //References all the textView which will pull that data from the database for each movie
+    TextView Plot, plotlistview, Date, runtime,  topquote, gross, datelistview, runtimelistview, topquotelistview, grosslistview;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
